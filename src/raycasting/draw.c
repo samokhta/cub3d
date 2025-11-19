2684eed05ca9824c44f7e0666ffb90ade6487d2a
Sael-Jad Mokhtari <samokhta@c1r4s4.42lausanne.ch>
2025-11-19 13:48:21 +0100

    merge

---
+++ src/raycasting/draw.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   draw.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sravizza <sravizza@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 17:42:40 by sravizza          #+#    #+#             */
+/*   Updated: 2025/11/18 09:41:18 by sravizza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+unsigned int	get_texture_pixel(void *texture, int x, int y)
+{
+	char			*addr;
+	int				bpp;
+	int				line_len;
+	int				endian;
+	unsigned int	color;
+
+	addr = mlx_get_data_addr(texture, &bpp, &line_len, &endian);
+	color = *(unsigned int *)(addr + (y * line_len + x * (bpp / 8)));
+	return (color);
+}
+
+void	draw_line(t_data *data, t_ray *ray, int x)
+{
+	int		y;
+	int		tex_y;
+	double	step;
+	double	tex_pos;
+	int		color;
+
+	step = (double)data->map.tex_size / ray->line_height;
+	tex_pos = (ray->draw_start - WINDOW_HEIGHT
+			/ 2 + ray->line_height / 2) * step;
+	y = ray->draw_start;
+	while (y < ray->draw_end)
+	{
+		tex_y = (int)tex_pos & (data->map.tex_size - 1);
+		tex_pos += step;
+		color = get_texture_pixel(ray->texture, ray->tex_x, tex_y);
+		ft_pixel_put(&data->img, x, y, color);
+		y++;
+	}
+}
---
+++ src/raycasting/get_texture.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   textures.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sravizza <sravizza@student.42lausanne.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/17 10:32:29 by sravizza          #+#    #+#             */
+/*   Updated: 2025/11/18 09:43:14 by sravizza         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	pick_texture(t_data *data, t_ray *ray)
+{
+	if (ray->side == 0)
+	{
+		if (ray->ray_dir_x > 0)
+			ray->texture = data->map.tex_ea;
+		else
+			ray->texture = data->map.tex_we;
+	}
+	else
+	{
+		if (ray->ray_dir_y > 0)
+			ray->texture = data->map.tex_so;
+		else
+			ray->texture = data->map.tex_no;
+	}
+}
+
+void	get_texture_coor(t_data *data, t_ray *ray)
+{
+	if (ray->side == 0)
+		ray->wall_x = data->player.y + ray->perp_wall_dist * ray->ray_dir_y;
+	else
+		ray->wall_x = data->player.x + ray->perp_wall_dist * ray->ray_dir_y;
+	ray->wall_x -= floor(ray->wall_x);
+	ray->tex_x = (int)(ray->wall_x * data->map.tex_size);
+	if ((ray->side == 0 && ray->ray_dir_x > 0)
+		|| (ray->side == 1 && ray->ray_dir_y < 0))
+		ray->tex_x = data->map.tex_size - ray->tex_x - 1;
+}
+
+void	get_texture(t_data *data, t_ray *ray)
+{
+	pick_texture(data, ray);
+	get_texture_coor(data, ray);
+}
