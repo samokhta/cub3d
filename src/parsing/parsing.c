c2a52a91aaed6cf033f3b8685da479444e81fc32
Piccolo-42 <stef.rvz@gmail.com>
2025-11-20 17:26:21 +0100

    norm

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -26,8 +26,6 @@ int	parsing(char *file, t_map *map)
 		return (close(fd), free_params(map), 0);
 	if (!get_player_coor(map))
 		return ((close(fd), free_params(map), 0));
-	// if (something_left(fd))
-	// 	return
 	close(fd);
 	if (!validate_map(map))
 		return (free_params(map), 0);
